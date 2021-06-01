c98bb512b4724dfee6a1f8ce69631ab87b7af92b
fredysomy <fredysomy@gmail.com>
2021-06-01 16:11:28 +0000

    Back Again

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from geoip2 import database
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
-from starlette.responses import HTMLResponse, UJSONResponse
+from starlette.responses import HTMLResponse, JSONResponse
 
-from middleware import WWWRedirectMiddleware
+#from middleware import WWWRedirectMiddleware
 
 app = Starlette()
 app.debug = False
@@ -18,7 +18,7 @@ app.debug = False
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_methods=["GET", "OPTIONS"]
 )
-app.add_middleware(WWWRedirectMiddleware)
+#app.add_middleware(WWWRedirectMiddleware)
 
 # Static
 app.mount("/static", StaticFiles(directory="static"))
@@ -46,9 +46,9 @@ async def geolocate(request):
     country_code = request.headers.get("CF-IPCountry", "").upper()
     country = app.countries.get(country_code)
     if country:
-        return UJSONResponse(country)
+        return JSONResponse(country)
     else:
-        return UJSONResponse({"message": "Could not geocode request."})
+        return JSONResponse({"message": "Could not geocode request."})
 
 
 @app.route("/api/geolocate/{ip}", methods=["GET", "HEAD"])
@@ -56,14 +56,14 @@ async def geolocate_ip(request):
     r = app.geoip2.country(request.path_params["ip"])
     country = app.countries.get(r.country.iso_code)
     if country:
-        return UJSONResponse(country)
+        return JSONResponse({"latitude":country["geo"]["latitude"],"longitude":country["geo"]["longitude"]})
     else:
-        return UJSONResponse({"message": "Could not geocode request."})
+        return JSONResponse({"message": "Could not geocode request."})
 
 
 @app.route("/api/countries", methods=["GET", "HEAD"])
 async def countries_list(request):
-    return UJSONResponse(app.countries)
+    return JSONResponse(app.countries)
 
 
 @app.route("/api/countries/{country_code}", methods=["GET", "HEAD"])
@@ -71,9 +71,9 @@ async def countries_detail(request):
     country_code = request.path_params["country_code"].upper()
     country = app.countries.get(country_code)
     if country:
-        return UJSONResponse(country)
+        return JSONResponse(country)
     else:
-        return UJSONResponse({"message": "Country code not found."})
+        return JSONResponse({"message": "Country code not found."})
 
 
 if __name__ == "__main__":
